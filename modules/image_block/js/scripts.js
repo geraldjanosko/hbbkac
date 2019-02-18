b5957fcdba89ad3223be2a3d87ebfcffab161a15
Gerald Janosko <Gerald_Janosko@external.mckinsey.com>
2019-02-18 00:34:02 -0500

    Theme updates for new bootstrap.

--- modules/image_block/js/scripts.js
+++ modules/image_block/js/scripts.js
@@ -1,6 +1,6 @@
 (function($){
 	var defaultURL = drupalSettings.path.baseUrl + drupalSettings.path.themeUrl + '/images/default.jpg';
-  $( ".image-block-img" ).error(function() {
+	$( ".image-block-img" ).on('error', function() {
     $(this).attr("src", defaultURL);
   })
 }(jQuery));
